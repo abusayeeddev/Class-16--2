6490d0ca9dd32807e90d3f4864766a4a7672f78a
Abu Sayeed <abusayeed483698@gmail.com>
2025-12-10 12:18:15 -0800

    class 16 -2

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('.benner').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  prevArrow: '<i class="fa-solid fa-arrow-left prev"></i>',
+  nextArrow: '<i class="fa-solid fa-arrow-right next"></i>',
+  dots:true,
+})
+
